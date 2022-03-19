83e509b33b409df64c8fa97bf767a94c008bf2cf
badal-1 <81519542+badal-1@users.noreply.github.com>
2022-03-19 08:31:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function n1(){
+window.location.href="1.html";
+}
+function n2(){
+window.location.href="2.html";
+}
+function n3(){
+window.location.href="3.html";
+}
+function n4(){
+window.location.href="4.html";
+}
+function n5(){
+window.location.href="5.html";
+}
